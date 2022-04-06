7012516c56fd2460a525a80ba2b25c1fbfc38a49
rakib-xs <hrakib435@gmail.com>
2022-04-06 15:01:18 +0600

    CSS Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,54 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
+(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        // Nav Toggle
+        function navToggle() {
+            $('#open-nav-btn').click(function () {
+                $('.main-nav-container').css({
+                    'width': '100%',
+                    'left': '0',
+                });
+            });
+            $('#close-nav-btn').click(function () {
+                $('.main-nav-container').css({
+                    'width': '0%',
+                    'left': '-75px',
+                });
+            });
+        }
+        navToggle();
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+        // Subscription Form
+        function isEmail(email) {
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(email);
+        }
+        function subscriptionForm() {
+            $('.subs-btn').click(function (e) {
+                e.preventDefault();
+                var emailField = $('.subscribe-form input[type="email"]');
+                var email = emailField.val();
+
+                if (email != '') {
+                    if (!isEmail(email)) {
+                        emailField.after("<span class='error-msg'>Whoops, make sure it's an email</span>");
+                        emailField.addClass('error');
+                        setTimeout(function () {
+                            $('.error-msg').remove();
+                        }, 2000);
+                    } else {
+                        emailField.after("<span class='success-msg'>Thanks for submitting..</span>");
+                        emailField.addClass('success');
+                        emailField.val('');
+                        setTimeout(function () {
+                            $('.success-msg').remove();
+                        }, 2000);
+
+                    }
+                }
+            }
+            );
+        }
+        subscriptionForm();
+    });
+})(jQuery);
